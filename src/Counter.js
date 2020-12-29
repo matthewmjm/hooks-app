d724414d6e68fb1add9f2a31e7bb3ad851eba71a
matthewmjm <matthewmjm@yahoo.com>
2020-12-28 21:55:54 -0700

    set up counter, increment and decrement before custom hook is created

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Counter() {
     const [count, setCount] = useState(0)
+
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+
+    useEffect(() => {
+        let id = setInterval(() => setCount(count + 1), 1000)
+        return () => clearInterval(id)
+    }, [count])
+
     return (
     <div>
         <h1>COUNT</h1>
         <p>The current count is: {count}</p>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
     </div>
     )
 }
