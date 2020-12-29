d944750b0a4b8d264d35b02a8676cbeb3089a5bc
matthewmjm <matthewmjm@yahoo.com>
2020-12-29 10:25:04 -0700

    add a decrement button to the demonstration

--- src/Demonstration.js
+++ src/Demonstration.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { useCounter } from './hooks/customHooks';
 
 export default function Demonstration() {
-    const [count] = useCounter(0)
+    const [count, , decrement] = useCounter(0)
     return (
         <div>
             <h2>This is a demonstration</h2>
             <h3>This component was mounted {count} seconds ago.</h3>
+            <button onClick={decrement}> - </button>
         </div>
     )
 }
